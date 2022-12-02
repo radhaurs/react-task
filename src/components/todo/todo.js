a3d874a6cc85d6985308c8f4ebdcf9b708e36d29
radhammab <radhammab.in@mouritech.com>
2022-12-02 11:49:05 +0530

    commit with edit and delete

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -2,22 +2,26 @@
 import React, { useState } from "react";
  
 const Todo = () => {
- const [showForm, setshowform] = useState(true);
- const [showNew, setshowNew] = useState(true);
- const [setshowDelete] = useState(true);
- const [toggleSubmit] = useState(true);
- const [isEditItem] = useState(null);
- const [showList, setshowList] = useState(true);
- const [deleteMessage] = useState(false);
- const [inputTitle, setinputTitle] = useState("");
- const [inputDesc, setinputDesc] = useState("");
+  const [showForm, setshowform] = useState(true);
+  const [showNew, setshowNew] = useState(true);
+  const [showDelete, setshowDelete] = useState(true);
+  const [toggleSubmit, settoggleSubmit] = useState(true);
+  const [isEditItem, setisEditItem] = useState(null);
+  const [showList, setshowList] = useState(true);
+  // const [editMessage, seteditMessage] = useState(false);
+  const [deleteMessage, setdeleteMessage] = useState(false);
+  const [ setdeleteMessagesuccess] = useState(false);
+  const [inputTitle, setinputTitle] = useState("");
+  const [inputDesc, setinputDesc] = useState("");
  const [inputDate, setinputDate] = useState("");
+ const [inputTime, setinputTime] = useState("");
  const [items, setitems] = useState([
    {
      id: "001",
      name: "Default Task",
      desc: "Default Description",
      date: "12-12-2022",
+     time: "12:12:12",
      status: false,
    },
  ]);
@@ -31,6 +35,9 @@ const Todo = () => {
  };
  const handleInputDate = (e) => {
   setinputDate(e.target.value);
+};
+const handleInputTime = (e) => {
+  setinputTime(e.target.value);
 };
  //   HANDLING INPUT FIELDS
  
@@ -56,6 +63,7 @@ const Todo = () => {
      setinputTitle("");
      setinputDesc("");
      setinputDate("");
+     setinputTime("");
     //  settoggleSubmit(true);
      setshowform(false);
      setshowDelete(true);
@@ -70,6 +78,7 @@ const Todo = () => {
      setinputTitle("");
      setinputDesc("");
      setinputDate("");
+     setinputTime("");
      setshowform(false);
    }
  };
@@ -83,6 +92,43 @@ const Todo = () => {
    setshowNew(false);
  };
  // ADD NEW TASK
+ //   DELETE
+ const handleDelete = (index) => {
+  console.log(index);
+  const updatedItems = items.filter((elem) => {
+    return index !== elem.id;
+  });
+  setdeleteMessage(true);
+
+  setTimeout(() => {
+    setitems(updatedItems);
+    setdeleteMessage(false);
+  }, 2000);
+  setdeleteMessagesuccess(false);
+};
+//   DELETE
+
+//   EDIT
+const handleEdit = (id) => {
+  setshowList(false);
+  setshowDelete(false);
+  setshowNew(false);
+  setshowform(true);
+
+  settoggleSubmit(false);
+  let newEditItem = items.find((elem) => {
+    return elem.id === id;
+  });
+  setinputTitle(newEditItem.name);
+  setinputDesc(newEditItem.desc);
+  setinputDate(newEditItem.date);
+  setinputTime(newEditItem.time);
+  // setshowDelete(true)
+
+  setisEditItem(id);
+  console.log(newEditItem);
+};
+//   EDIT
  return (
    <>
      {showNew ? (
@@ -116,7 +162,11 @@ const Todo = () => {
                <label className="my-2" htmlFor="description">
                  Enter Date
                </label>
-               <input type="date" name="description" id="description" placeholder="Description" className="w-100 my-1 p-2" onChange={handleInputDate} value={inputDate} />
+               <input type="date" name="date" id="date" placeholder="Date" className="w-100 my-1 p-2" onChange={handleInputDate} value={inputDate} />
+               <label className="my-2" htmlFor="description">
+                 Enter Time
+               </label>
+               <input type="time" name="time" id="time" placeholder="Time" className="w-100 my-1 p-2" onChange={handleInputTime} value={inputTime} />
                <button className="btn btn-primary my-2">Save</button>
              </form>
            </div>
@@ -128,30 +178,46 @@ const Todo = () => {
  
      {showList ? (
        <div className="container py-2 ">
-         {deleteMessage ? (
-           <p className="text-center text-danger">Item Deleted Successfully</p>
-         ) : (
-           ""
-         )}
-         {items.map((elem, index) => {
-           return (
-             <div
-               className="row border rounded shadow p-3 mb-3 bg-white rounded  p-2"
-               key={elem.id}
-             >
-               <div className="col-12 d-flex justify-content-between align-items-center">
-                 <div>
-                   <h4>{elem.name}</h4>
-                   <p>{elem.desc}</p>
-                   <p>{elem.date}</p>
-                 </div>
-                   
-                 </div>
+       {deleteMessage ? (
+         <p className="text-center text-danger">Item Deleted Successfully</p>
+       ) : (
+         ""
+       )}
+       {items.map((elem, index) => {
+         return (
+           <div
+             className="row border rounded shadow p-3 mb-3 bg-white rounded  p-2"
+             key={elem.id}
+           >
+             <div className="col-12 d-flex justify-content-between align-items-center">
+               <div>
+                 <h4>{elem.name}</h4>
+                 <p>{elem.desc}</p>
+                 <p>{elem.date}</p>
+                 <p>{elem.time}</p>
                </div>
-            
-           );
-         })}
-       </div>
+                 <button
+                   className="btn btn-primary mx-2"
+                   onClick={() => handleEdit(elem.id)}
+                 >
+                   Edit
+                 </button>
+                 {showDelete ? (
+                   <button
+                     className="btn btn-danger mx-2"
+                     onClick={() => handleDelete(elem.id)}
+                   >
+                     Delete
+                   </button>
+                 ) : (
+                   ""
+                 )}
+               </div>
+             </div>
+          
+         );
+       })}
+     </div>
      ) : (
        ""
      )}
